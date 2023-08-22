cb8e41428f299442316c829567b9530bc4ea9836
Abdallah Shehata <abdallah.elsayed@squadio.com>
2023-08-22 06:38:58 +0300

    setup required dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -30,11 +32,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -51,7 +53,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
@@ -66,4 +68,65 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    kapt ("com.android.databinding:compiler:3.1.4")
+    implementation ("androidx.activity:activity-ktx:1.7.2")
+    implementation ("androidx.fragment:fragment-ktx:1.6.1")
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+
+    val lifecycle_version = "2.6.1"
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+
+
+    // Unit Test
+    testImplementation( "org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+    testImplementation( "io.mockk:mockk:1.13.5")
+    testImplementation( "android.arch.core:core-testing:1.1.1")
+    testImplementation ("org.mockito:mockito-inline:2.13.0")
+    testImplementation( "org.robolectric:robolectric:4.7.3")
+    testImplementation( "androidx.test:runner:1.5.2")
+    testImplementation( "androidx.test.ext:junit:1.1.5")
+    testImplementation( "androidx.test.espresso:espresso-core:3.5.1")
+    testImplementation( "app.cash.turbine:turbine:0.13.0")
+    testImplementation( "org.jetbrains.kotlin:kotlin-test:1.8.21")
+
+    // Networking
+    implementation( "com.squareup.okhttp3:okhttp:5.0.0-alpha.2")
+    implementation( "com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.11")
+    implementation( "com.squareup.retrofit2:retrofit:2.9.0")
+    implementation( "com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.retrofit2:adapter-rxjava2:2.4.0")
+
+    //Glide
+    implementation( "com.github.bumptech.glide:glide:4.15.1")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.15.1")
+
+    //Facebook shimmer
+    implementation( "com.facebook.shimmer:shimmer:0.5.0")
+
+    //Lottie files
+    implementation( "com.airbnb.android:lottie:3.4.0")
+
+    //dependency injection
+    val hiltVersion="2.47.0"
+    implementation("com.google.dagger:hilt-android:$hiltVersion")
+    kapt("com.google.dagger:hilt-android-compiler:$hiltVersion")
+
+    //coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    //RxJava
+    implementation("io.reactivex.rxjava3:rxandroid:3.0.2")
+    implementation("io.reactivex.rxjava3:rxjava:3.1.5")
+
+    //coil
+    implementation("io.coil-kt:coil:2.3.0")
 }
